d49256901d117b908807a61abfd01568fbba77b0
Yanael Barbier <y.barbier@sprout.ai>
2023-09-11 23:40:32 +0100

    wip, parse list

--- index.ts
+++ index.ts
@@ -5,22 +5,47 @@ const openai = new OpenAI({
 });
 
 
-const chatCompletion = await openai.chat.completions.create({
-    messages: [
-        { 
-            role: "user", 
-            content: "Say this is a test" 
-        }],
-    model: "gpt-3.5-turbo",
-    stream: true,
-});
 
-export function sayHello(name: string):string {
+let buffer = "";
+let list = [];
 
-    console.log(chatCompletion);
-
-    return `Hello via Bun ${name}`;
+async function streamParser(content: any) {
+    
+    buffer += content;
+    // List parser
+    if (buffer.startsWith("- ") && buffer.endsWith("\n")) {
+        let s = buffer.slice(2, -1);
+        list.push(s);
+        buffer = "";
+        process.stdout.write(s);
+        process.stdout.write("\n");
+    } 
 }
 
-const res = sayHello("jay");
-console.log(res);
+
+export async function main(name: string) {
+
+    const stream = await openai.chat.completions.create({
+        messages: [
+            { 
+                role: "user", 
+                content: "Give me a list of 5 cities in california. Bullet point them." 
+            }],
+        model: "gpt-3.5-turbo",
+        stream: true,
+    });
+
+    for await (const msg of stream) {
+        // console.log(msg);
+        const content = msg.choices[0].delta.content
+
+        // Print without new line:
+        if (content) {
+            streamParser(content);
+        }
+    }
+    process.stdout.write("\n");
+
+}
+
+main();
