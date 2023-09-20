1a1d8927cf3071dc5d63d555ffc18797b51c8af4
Yanael Barbier <y.barbier@sprout.ai>
2023-09-20 12:03:52 +0100

    Add Open Ai Handler

---
+++ src/openAiHandler.ts
@@ -0,0 +1,58 @@
+import { StreamMode, StreamResponseWrapper, Status } from "./utils.js";
+
+import { StreamParser } from "./streamParser5.js";
+
+
+export class OpenAiHandler {
+
+    private itemIdx = 0;
+    private noStreamBufferList: any = [];
+    private parser: StreamParser;
+    private mode: StreamMode;
+
+    constructor(mode = StreamMode.StreamObjectKeyValueTokens) {
+        this.mode = mode;
+        this.parser = new StreamParser(this.mode);
+    }
+
+    async *process(stream: any) {
+
+        for await (const msg of stream) {
+
+            let content = "";
+
+            content = msg.choices[0].delta.content;
+
+            if (!content) {
+                content = msg.choices[0]?.delta?.function_call?.arguments + "";
+            }
+
+            const res = this.parser.parse(content);;
+
+            if (this.mode === StreamMode.NoStream || this.mode === StreamMode.Batch) {
+            
+                if (res) {
+                    this.noStreamBufferList.push(res);
+                }
+            } else if (res) {
+                yield res;
+            }
+        }
+
+        if (this.mode === StreamMode.NoStream) {
+            for (const item of this.noStreamBufferList) {
+                yield item;
+            }
+        } else if (this.mode === StreamMode.Batch) {
+            
+            const streamRes: StreamResponseWrapper = {
+                index: this.itemIdx,
+                status: Status.COMPLETED,
+                data: this.noStreamBufferList.map((item: any) => item.data),
+            };
+            yield streamRes;
+        }
+        yield null;
+    }
+
+}
