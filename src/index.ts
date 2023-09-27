dd1f4f13d1b0b535737a473d8081f4986bc18652
Yanael Barbier <y.barbier@sprout.ai>
2023-09-27 19:51:19 +0100

    Clean repo

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { StreamMode } from "./utils.js";
-export { StreamParser } from "./streamParser.js";
-export { OpenAiHandler } from "./openAiHandler.js";
-export { Entity } from "./entity.js";
