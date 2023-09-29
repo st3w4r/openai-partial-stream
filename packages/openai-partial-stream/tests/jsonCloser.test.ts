c527eabb894abc6976ad19da4bbb8751919f277c
Yanael Barbier <y.barbier@sprout.ai>
2023-09-29 11:41:39 +0100

    Add test

--- packages/openai-partial-stream/tests/jsonCloser.test.ts
+++ packages/openai-partial-stream/tests/jsonCloser.test.ts
@@ -1,10 +1,76 @@
-import {test, expect} from "@jest/globals";
-import {JsonCloser} from "../src/jsonCloser";
+// External Libraries
+import { test, expect } from "@jest/globals";
 
+// Internal Modules
+import { JsonCloser } from "../src/jsonCloser";
+import { StreamMode } from "../src/utils";
 
-test("JsonCloser", async () => {
-    const jsonCloser = new JsonCloser();
-    jsonCloser.append(`{"a": 1, "b": 2, "c": 3}`);
-    const closedJson = jsonCloser.closeJson();
-    expect(closedJson).toBe(`{"a": 1, "b": 2, "c": 3}`);
+let jsonCloser: JsonCloser;
+
+beforeEach(() => {
+    jsonCloser = new JsonCloser();
+});
+
+const runAndExpectClose = (input: string[], expected: string) => {
+    input.forEach(data => jsonCloser.append(data));
+    let closedJson = jsonCloser.closeJson();
+    expect(closedJson).toBe(expected);
+};
+
+test("handles complete JSON", async () => {
+    runAndExpectClose([`{"a": 1, "b": 2, "c": 3}`], `{"a": 1, "b": 2, "c": 3}`);
+});
+
+test("handles partial JSON with missing parenthese", async () => {
+    runAndExpectClose([`{"a": 1, "b": 2`], `{"a": 1, "b": 2}`);
+});
+
+test("handles partial JSON with missing quote", async () => {
+    runAndExpectClose([`{"a": 1, "b`], `{"a": 1, "b}`);
+});
+
+test("closes multi-token partial JSON", async () => {
+    runAndExpectClose([`{"a": 1, "b`, `": 2}`], `{"a": 1, "b": 2}`);
+});
+
+test("closes empty JSON object", async () => {
+    runAndExpectClose(['{', '}'], `{}`);
+});
+
+test("closes empty JSON array", async () => {
+    runAndExpectClose(['[', ']'], `[]`);
+});
+
+test("closes multi-token JSON with incomplete string", async () => {
+    runAndExpectClose([`{"a": 1, "b": "ok`], `{"a": 1, "b": "ok}`);
+});
+
+test("closes multi-token JSON with StreamMode", async () => {
+    jsonCloser = new JsonCloser(StreamMode.StreamObjectKeyValueTokens);
+    runAndExpectClose([`{"a": 1, "b": "ok`], `{"a": 1, "b": "ok"}`);
+});
+
+test("closes nested multi-token JSON with StreamMode", async () => {
+    jsonCloser = new JsonCloser(StreamMode.StreamObjectKeyValueTokens);
+    runAndExpectClose([`{"a": 1, "b": {"ok": "super`], `{"a": 1, "b": {"ok": "super"}}`);
+});
+
+test("closes partial JSON array with nested objects", async () => {
+    runAndExpectClose([`[{"a": 1,`,` "b":`, ` {"ok": "super"`], `[{"a": 1, "b": {"ok": "super"}}]`);
+});
+
+test("close partial JSON many appends", async () => {
+    runAndExpectClose([
+        `{`,
+        `"`,
+        `a"`,
+        `: 1`,
+        `, "`,
+        `b"`,
+        `: {"o`,
+        `k`,
+        `": "super`,
+        `"`,
+        `}}`,
+    ], `{"a": 1, "b": {"ok": "super"}}`);
 });
